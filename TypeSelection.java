7c7bb825e91db65052b54588930c1540870ac158
heewon5602 <112792949+heewon5602@users.noreply.github.com>
2023-06-01 03:18:13 +0900

    Upload second java file
    This is converted code properly along the xml file.

--- TypeSelection.java
+++ TypeSelection.java
@@ -1,52 +1,48 @@
 /*사용자의 유형(고객, 배달원, 가게주인) 선택창 (회원가입의 첫번째 단계)*/
 package com.example.breakthedelivery;
 
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class TypeSelection extends AppCompatActivity {
-    TextView txtType; //User Type Selecton text의 id
-    Button btnCustomer; //Custoer button의 아이디
-    Button btnDelivery; //Delivery Man의 아이디
-    Button btnStore; //Store Owner의 아이디
+    Button customer_select; //Customer button의 아이디
+    Button deliverMan_select; //Delivery Man의 아이디
+    Button storeOwner_select; //Store Owner의 아이디
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_type_selection);
+        setContentView(R.layout.signup_select);
 
-        txtType = (TextView)findViewById(R.id.txtType);
-        btnCustomer = findViewById(R.id.customer); //CustomerCreate로 향하는 button 'customer'의 id 가져옴
-        btnDelivery = findViewById(R.id.delivery); //DeliveryCreate로 향하는 button 'delivery'의 id 가져옴
-        btnStore = findViewById(R.id.store); //StoreCreate로 향하는 button 'store'의 id 가져옴
+        customer_select = findViewById(R.id.customer_select); //CustomerCreate로 향하는 button 'customer'의 id 가져옴
+        deliverMan_select = findViewById(R.id.deliverMan_select); //DeliveryCreate로 향하는 button 'delivery'의 id 가져옴
+        storeOwner_select = findViewById(R.id.storeOwner_select); //StoreCreate로 향하는 button 'store'의 id 가져옴
 
-        btnCustomer.setOnClickListener(new View.OnClickListener() {
+        customer_select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext().CustomerCreate.class);
-                StartActivityForResult(intent, CustomerCreate);
+                Intent intent = new Intent(getApplicationContext(), CustomerCreate.class);
+                startActivity(intent);
             }
         });
 
-        btnDelivery.setOnClickListener(new View.OnClickListener() {
+        deliverMan_select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext().DeliveryCreate.class);
-                StartActivityForResult(intent, DeliveryCreate);
+                Intent intent = new Intent(getApplicationContext(), DeliveryCreate.class);
+                startActivity(intent);
             }
         });
 
-        btnStore.setOnClickListener(new View.OnClickListener() {
+        storeOwner_select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext().StoreCreate.class);
-                StartActivityForResult(intent, StoreCreate);
+                Intent intent = new Intent(getApplicationContext(), CustomerCreate.class);
+                startActivity(intent);
             }
         });
     }
