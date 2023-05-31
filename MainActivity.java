6a06f003bf4f2f2826031f2251a1a46bc23b574a
heewon5602 <112792949+heewon5602@users.noreply.github.com>
2023-06-01 03:23:53 +0900

    Upload second java file
    This is converted code along the xml file.

--- MainActivity.java
+++ MainActivity.java
@@ -2,40 +2,38 @@
 
 package com.example.breakthedelivery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtBTD; //BreakTheDelivery라는 어플 이름
-    Button btnSignIn; //SignIn UI로 넘어가는 버튼
-    Button btnCreate; //TypeSelection UI로 넘어가 type선택을 도와줌 (계정생성 버튼)
+    Button signIn; //SignIn UI로 넘어가는 버튼
+    Button signUp; //TypeSelection UI로 넘어가 type선택을 도와줌 (계정생성 버튼)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        txtBTD = (TextView)findViewById(R.id.txtBTD);
-        btnSignIn = findViewById(R.id.signIn); //SignIn UI로 향하는 button 'signIn'의 id 가져옴
-        btnCreate = findViewById(R.id.create); //TypeSelection으로 향하는 button 'create'의 id 가져옴
+        setContentView(R.layout.start_screen);
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        signIn = findViewById(R.id.signIn); //SignIn UI로 향하는 button 'signIn'의 id 가져옴
+        signUp = findViewById(R.id.signUp); //TypeSelection으로 향하는 button 'signUp'의 id 가져옴
+
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext().SignIn.class);
-                StartActivityForResult(intent, SignIn);
+                Intent intent = new Intent(getApplicationContext(), SignIn.class);
+                startActivity(intent);
             }
         });
 
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext().TypeSelection.class);
-                StartActivityForResult(intent, TypeSelection);
+                Intent intent = new Intent(getApplicationContext(), TypeSelection.class);
+                startActivity(intent);
             }
         });
     }
