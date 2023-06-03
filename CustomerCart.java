1fe32f840aec687e77a6c901c409431773253932
heewon5602 <112792949+heewon5602@users.noreply.github.com>
2023-06-03 10:00:36 +0900

    Second Upload
    Conncection with xml and some edits which are matched with xml are existed.

--- CustomerCart.java
+++ CustomerCart.java
@@ -1,63 +1,114 @@
 package com.example.breakthedelivery;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CustomerCart extends AppCompatActivity {
-    Button btnHome; //하단바의 홈 버튼
-    Button btnParticipate; //하단바의 참여 버튼
-    Button btnDelivery; //하단바의 배달 버튼
-    Button btnMyPage; //하단바의 마이페이지 버튼
-    Button btnCart; //상단바의 장바구니 버튼
+    ImageButton home_button; //하단바의 홈 버튼
+    ImageButton group_button; //하단바의 참여 버튼
+    ImageButton delivery_button; //하단바의 배달 버튼
+    ImageButton profile_button; //하단바의 마이페이지 버튼
+    ImageButton btnCart; //상단바의 장바구니 버튼
+    private ImageButton plus1;
+    private ImageButton plus2;
+    private ImageButton plus3;
+    private ImageButton minus1;
+    private ImageButton minus2;
+    private ImageButton minus3;
 
     /*담은 메뉴에 대한 parameters*/
     Button btnDelete; //메뉴 삭제 버튼
-    Button btnAdd;
-    Button btnMinus;
-    Button btnOrder; //주문하기 버튼
-    TextView txtCount; //메뉴 수량
-    TextView txtMenu; //메뉴 이름
-    private int count = 0;
-
+    private ImageButton plus;
+    private ImageButton minus;
+    private Button btnOrder; //주문하기 버튼
+    private TextView count1, count2, count3; //수량을 보여주는 TextView
+    private int value1 = 0;
+    private int value2 = 0;
+    private int value3 = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_cart);
+        setContentView(R.layout.customer_cart);
 
-        btnHome = (Button) findViewById(R.id.btnHome);
-        btnParticipate = (Button) findViewById(R.id.btnParticipate);
-        btnDelivery = (Button) findViewById(R.id.btnDelivery);
-        btnMyPage = (Button) findViewById(R.id.btnMypage);
+        home_button = (ImageButton)findViewById(R.id.home_button);
+        group_button = (ImageButton)findViewById(R.id.group_button);
+        delivery_button = (ImageButton)findViewById(R.id.delivery_button);
+        profile_button = (ImageButton)findViewById(R.id.profile_button);
         btnCart = (Button) findViewById(R.id.btnCart);
-        txtCount = findViewById(R.id.txtCount);
-        txtCount.setText(count+"");
-        btnAdd = findViewById(R.id.btn_add);
-        btnMinus = findViewById(R.id.btn_minus);
-        btnOrder = findViewById(R.id.btnOrder);
-        private int duration = Toast.LENGTH_SHORT;
+        count1 = (TextView) findViewById(R.id.count1);
+        count1.setText(value1+"");
+        count2 = (TextView) findViewById(R.id.count2);
+        count2.setText(value2+"");
+        count3 = (TextView) findViewById(R.id.count3);
+        count3.setText(value3+"");
+        plus1 = (ImageButton) findViewById(R.id.plus1);
+        plus2 = (ImageButton) findViewById(R.id.plus2);
+        plus3 = (ImageButton) findViewById(R.id.plus3);
+        minus1 = (ImageButton) findViewById(R.id.minus1);
+        minus2 = (ImageButton) findViewById(R.id.minus2);
+        minus3 = (ImageButton) findViewById(R.id.minus3);
+        btnOrder = (ImageButton)findViewById(R.id.btnOrder);
+
+        /*장바구니 버튼*/
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnCart.setEnabled(false);
+            }
+        }); //같은 공간이므로 버튼 비활성화
 
         /*수량 증가 및 감소, 삭제 버튼들의 기능*/
-        btnAdd.setOnClickListener(new View.OnClickListener() {
+        plus1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count++;
-                txtCount.setText(count+"");
+                    value1++;
+                    count1.setText(value1+"");
+            }
+        });
+        plus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value2++;
+                count2.setText(value2+"");
+            }
+        });
+        plus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value3++;
+                count3.setText(value3+"");
             }
         });
 
-        btnMinus.setOnClickListener(new View.OnClickListener() {
+        minus1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count--;
-                txtCount.setText(count+"");
+                value1--;
+                count1.setText(value1+"");
+            }
+        });
+        minus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value2--;
+                count2.setText(value2+"");
+            }
+        });
+        minus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value3--;
+                count3.setText(value3+"");
             }
         });
 
@@ -78,7 +129,7 @@ public class CustomerCart extends AppCompatActivity {
         });
 
         /*하단바 메뉴 버튼들의 기능*/
-        btnHome.setOnClickListener(new View.OnClickListener() {
+        home_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CustomerHome.class);
@@ -86,7 +137,7 @@ public class CustomerCart extends AppCompatActivity {
             }
         });
 
-        btnParticipate.setOnClickListener(new View.OnClickListener() {
+        group_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CustomerParticipate.class);
@@ -94,7 +145,7 @@ public class CustomerCart extends AppCompatActivity {
             }
         });
 
-        btnDelivery.setOnClickListener(new View.OnClickListener() {
+        delivery_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CustomerMyDelivery.class);
@@ -102,19 +153,12 @@ public class CustomerCart extends AppCompatActivity {
             }
         });
 
-        btnMyPage.setOnClickListener(new View.OnClickListener() {
+        profile_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CustomerMyPage.class);
                 startActivity(intent);
             }
         });
-
-        btnCart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                btnCart.setEnabled(false);
-            }
-        }); //같은 공간이므로 버튼 비활성화
     }
 }
