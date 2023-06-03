ae51a1afe04655f4440c97ce7801fb8a1ade027b
heewon5602 <112792949+heewon5602@users.noreply.github.com>
2023-06-04 02:36:35 +0900

    Second Upload
    Match codes with xml files' codes.

--- CustomerOrderHistory.java
+++ CustomerOrderHistory.java
@@ -1,24 +1,60 @@
 package com.example.breakthedelivery;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CustomerOrderHistory extends AppCompatActivity {
-    TextView txtOrderHistory; //'주문내역' 상단탭에 출력
-    TextView txtStore; //가게이름 출력
-    TextView txtOrder; //주문내역 출력
-    TextView txtSaving; //절약현황 출력
+
+    ImageButton home_button; //하단바의 홈 버튼
+    ImageButton group_button; //하단바의 참여 버튼
+    ImageButton delivery_button; //하단바의 배달 버튼
+    ImageButton profile_button; //하단바의 마이페이지 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_order_history);
+        setContentView(R.layout.customer_order_history);
 
-        txtOrderHistory = (TextView) findViewById(R.id.txtOrderHistory);
-        txtStore = (TextView) findViewById(R.id.txtStore);
-        txtOrder = (TextView) findViewById(R.id.txtOrder);
-        txtSaving = (TextView) findViewById(R.id.txtSavin);
+        home_button = (ImageButton)findViewById(R.id.home_button);
+        group_button = (ImageButton)findViewById(R.id.group_button);
+        delivery_button = (ImageButton)findViewById(R.id.delivery_button);
+        profile_button = (ImageButton)findViewById(R.id.profile_button);
+
+        /*하단바 메뉴 버튼들의 기능*/
+        home_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CustomerHome.class);
+                startActivity(intent);
+            }
+        });
+
+        group_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CustomerParticipate.class);
+                startActivity(intent);
+            }
+        });
+
+        delivery_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CustomerMyDelivery.class);
+                startActivity(intent);
+            }
+        });
+
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CustomerMyPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
