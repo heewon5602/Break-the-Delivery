03b4136c1a0ba38826ff51adb9fb74e97c4af4e5
heewon5602 <112792949+heewon5602@users.noreply.github.com>
2023-06-03 09:56:32 +0900

    Second Upload
    Connections with xml and some edits which are matched with xml are existed.

--- CustomerParticipateStatus.java
+++ CustomerParticipateStatus.java
@@ -11,6 +11,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,88 +28,129 @@ public class CustomerParticipateStatus extends AppCompatActivity {
     private Context context;
     private RecyclerView.Adapter adapter;
 
-    int count = 1; //생성자는 참여 상태이므로 1로 초기화해줌
+    ImageButton home_button; //하단바의 홈 버튼
+    ImageButton group_button; //하단바의 참여 버튼
+    ImageButton delivery_button; //하단바의 배달 버튼
+    ImageButton profile_button; //하단바의 마이페이지 버튼
+    ImageButton btnCart; //상단바의 장바구니 버튼
     EditText edtSearch; //검색어를 입력할 검색창
-    TextView txtBTD; //BreakTheDelivery라는 어플 이름
-    TextView txtStorename; //가게이름
-    TextView txtOrderTime; //주문 시간
-    TextView txtNumber; //참여 인원
-    TextView txtTotalNumber; //생성자가 설정한 총 참여 가능 인원 수
-    Button btnParticipate; //참여 버튼
+    private TextView tv1_1; //'현재 참여 인원/총 참여 가능 인원'
+    private TextView tv; // '현재 참여 인원/총 참여 가능 인원'
+    private Button button_participate1_1; //참여하기 버튼
+    private Button button_participate; //참여하기 버튼
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_participate_status);
+        setContentView(R.layout.customer_participate_status);
         this.context = getApplicationContext();
 
-        txtBTD = (TextView)findViewById(R.id.btnBTD);
+        home_button = (ImageButton)findViewById(R.id.home_button);
+        group_button = (ImageButton)findViewById(R.id.group_button);
+        delivery_button = (ImageButton)findViewById(R.id.delivery_button);
+        profile_button = (ImageButton)findViewById(R.id.profile_button);
+        btnCart = (ImageButton) findViewById(R.id.btnCart);
         edtSearch = (EditText) findViewById(R.id.Search); //xml에서 search 라는 id를 가진 EditText layout 가져오기
-        txtStorename = (TextView)findViewById(R.id.Storename); //가게 이름
-        txtOrderTime = (TextView)findViewById(R.id.OrderTime); //주문할 시간
-        txtNumber = (TextView)findViewById(R.id.Number); //현재 참여된 인원 수
-        txtTotalNumber = (TextView)findViewById(R.id.TotalNumber); //모집할 인원 수
-        btnParticipate = (Button)findViewById(R.id.btnParticipate); //참여 버튼
+        tv1_1 = (TextView)findViewById(R.id.tv1_1);
+        int now2 = 1;
+        int all2 = 0; //임시로 0으로 해둠(db에서 가져와야함)
+        tv1_1.setText(now1_1+"/"+all1_1);
+        tv = (TextView)findViewById(R.id.tv);
+        int now = 1;
+        int all = 0; //임시
+        tv.setText(now +"/"+all);
+        button_participate1_1 = (Button)findViewById(R.id.button_participate1_1);
+        button_participate = (Button)findViewById(R.id.button_participate);
 
-        btnParticipate.setOnClickListener(new View.OnClickListener() {
+        /*참여 버튼*/
+        button_participate1_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (btnParticipate.getText() == "참여") {
-                    count++;
-                    txtNumber.setText("");
-                    btnParticipate.setText("참여 취소");
-                    Toast.makeText(CustomerParticipateStatus.this, "참여되었습니다", Toast.LENGTH_SHORT).show();
-                } //참여 버튼 클릭 : 참여 인원 수 증가, 버튼 텍스트 변경, 토스트 메시지 출력
-                else {
-                    count--;
-                    txtNumber.setText("");
-                    btnParticipate.setText("참여");
-                    Toast.makeText(CustomerParticipateStatus.this, "참여 취소되었습니다", Toast.LENGTH_SHORT).show();
-                } //참여 취소 버튼 클릭 : 참여 인원 수 감소, 버튼 텍스트 변경, 토스트 메시지 출력
-
-               if (txtNumber == txtTotalNumber) btnParticipate.setEnabled(false);
-                // 참여 가능 인원 수와 참여 인원 수가 동일해지면 버튼 비활성화됨
-                else btnParticipate.setEnabled(true); // 참여 가능 인원 수와 참여 인원 수가 다르면 버튼 활성화됨
+                if (now1_1 == all1_1) {
+                    Toast.makeText(CustomerParticipateStatus.this, "인원 모집이 마감되었습니다. 다른 참여방을 찾아주세요.", Toast.LENGTH_LONG).show();
+                    // 참여 가능 인원 수와 참여 인원 수가 동일해지면 버튼 비활성화됨
+                } else{
+                    if (button_participate1_1.getText() == "참여") {
+                        now1_1++;
+                        tv1_1.setText(now1_1 +"/"+all1_1);
+                        button_participate1_1.setText("참여 취소");
+                        Toast.makeText(CustomerParticipateStatus.this, "참여되었습니다", Toast.LENGTH_SHORT).show();
+                    } //참여 버튼 클릭 : 참여 인원 수 증가, 버튼 텍스트 변경, 토스트 메시지 출력
+                    else {
+                        now1_1--;
+                        tv1_1.setText(now1_1+"/"+all1_1);
+                        button_participate1_1.setText("참여");
+                        Toast.makeText(CustomerParticipateStatus.this, "참여 취소되었습니다", Toast.LENGTH_SHORT).show();
+                    } //참여 취소 버튼 클릭 : 참여 인원 수 감소, 버튼 텍스트 변경, 토스트 메시지 출력
+                }
             }
         });
 
-        edtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        button_participate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if(i == EditorInfo.IME_ACTION_SEARCH) {
-                    return true;
-                } return false;
+            public void onClick(View view) {
+                if (now == all) {
+                    Toast.makeText(CustomerParticipateStatus.this, "인원 모집이 마감되었습니다. 다른 참여방을 찾아주세요.", Toast.LENGTH_LONG).show();
+                    // 참여 가능 인원 수와 참여 인원 수가 동일해지면 버튼 비활성화됨
+                } else{
+                    if (button_participate.getText() == "참여") {
+                        now++;
+                        tv1_1.setText(now +"/"+all);
+                        button_participate.setText("참여 취소");
+                        Toast.makeText(CustomerParticipateStatus.this, "참여되었습니다", Toast.LENGTH_SHORT).show();
+                    } //참여 버튼 클릭 : 참여 인원 수 증가, 버튼 텍스트 변경, 토스트 메시지 출력
+                    else {
+                        now--;
+                        tv1_1.setText(now+"/"+all);
+                        button_participate.setText("참여");
+                        Toast.makeText(CustomerParticipateStatus.this, "참여 취소되었습니다", Toast.LENGTH_SHORT).show();
+                    } //참여 취소 버튼 클릭 : 참여 인원 수 감소, 버튼 텍스트 변경, 토스트 메시지 출력
+                }
             }
         });
 
-        //editText의 검색어 실시간 변화를 관찰하는 인터페이스
-
-        edtSearch.addTextChangedListener(new TextWatcher() {
+        /*하단바 메뉴 버튼*/
+        home_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int s, int n, int tn) {
+            public void onClick(View view) {
 
+                Intent intent = new Intent(getApplicationContext(), CustomerHome.class);
+                startActivity(intent);
             }
-            /* 텍스트 변경 전
-             charSequence : 기존 문자열, s: 커서 시작 위치, n: 변경 대상 문자 수, tn(total number): 변경 후 문자 수 */
+        }); //현재 Home screen 노출 중이므로, 비활성화시킴
 
+        group_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTextChanged(CharSequence charSequence, int s, int n, int tn) {
-                if(edtSearch.getText().length() >= 50)
-                    Toast.makeText(CustomerParticipateStatus.this, "20자 이내로 입력하세요", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CustomerParticipate.class);
+                startActivity(intent);
             }
-            /* 텍스트 변경 시
-            charSequence : 기존 문자열, s: 커서 시작 위치, n: 변경 대상 문자 수, tn(total number): 변경 후 문자 수
-             */
+        });
 
+        delivery_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void afterTextChanged(Editable editable) {
-                String search = edtSearch.getText().toString().trim(); //edtSearch의 텍스트를 공백 제외하고 가져와 search라는 변수에 문자열로 저장
-
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CustomerMyDelivery.class);
+                startActivity(intent);
             }
-            /* 텍스트 변경 후
-            editable: 변경된 문자열
-             */
+        });
+
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CustomerMyPage.class);
+                startActivity(intent);
+            }
+        });
+
+        /*장바구니 버튼*/
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CustomerCart.class);
+                startActivity(intent);
+            }
         });
     }
     }
