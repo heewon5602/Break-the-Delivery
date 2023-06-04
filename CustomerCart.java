2bf097188fa30ef9a590dd953a9aed35f06a81bf
heewon5602 <112792949+heewon5602@users.noreply.github.com>
2023-06-04 17:44:21 +0900

    Revise some codes
    Revise codes to conduct functions of button 'plus', 'minus', 'delete'.

--- CustomerCart.java
+++ CustomerCart.java
@@ -1,13 +1,11 @@
 package com.example.breakthedelivery;
 
-import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +15,13 @@ public class CustomerCart extends AppCompatActivity {
     ImageButton delivery_button; //하단바의 배달 버튼
     ImageButton profile_button; //하단바의 마이페이지 버튼
     ImageButton btnCart; //상단바의 장바구니 버튼
+    Button button_delete1_1;
+    Button button_delete1_2;
+    Button button_delete1_3;
+    FrameLayout menu1_1;
+    FrameLayout menu1_2;
+    FrameLayout menu1_3;
+
     private ImageButton plus1;
     private ImageButton plus2;
     private ImageButton plus3;
@@ -26,8 +31,6 @@ public class CustomerCart extends AppCompatActivity {
 
     /*담은 메뉴에 대한 parameters*/
     Button btnDelete; //메뉴 삭제 버튼
-    private ImageButton plus;
-    private ImageButton minus;
     private Button btnOrder; //주문하기 버튼
     private TextView count1, count2, count3; //수량을 보여주는 TextView
     private int value1 = 0;
@@ -40,24 +43,30 @@ public class CustomerCart extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_cart);
 
-        home_button = (ImageButton)findViewById(R.id.home_button);
-        group_button = (ImageButton)findViewById(R.id.group_button);
-        delivery_button = (ImageButton)findViewById(R.id.delivery_button);
-        profile_button = (ImageButton)findViewById(R.id.profile_button);
-        btnCart = (Button) findViewById(R.id.btnCart);
+        home_button = (ImageButton) findViewById(R.id.home_button);
+        group_button = (ImageButton) findViewById(R.id.group_button);
+        delivery_button = (ImageButton) findViewById(R.id.delivery_button);
+        profile_button = (ImageButton) findViewById(R.id.profile_button);
+        btnCart = (ImageButton) findViewById(R.id.btnCart);
+        button_delete1_1 = (Button) findViewById(R.id.button_delete1_1);
+        button_delete1_2 = (Button) findViewById(R.id.button_delete1_2);
+        button_delete1_3 = (Button) findViewById(R.id.button_delete1_3);
+        menu1_1 = (FrameLayout)findViewById(R.id.menu1_1);
+        menu1_2 = (FrameLayout)findViewById(R.id.menu1_2);
+        menu1_3 = (FrameLayout)findViewById(R.id.menu1_3);
         count1 = (TextView) findViewById(R.id.count1);
-        count1.setText(value1+"");
+        count1.setText("수량: 1"); //초기 수량 임의 설정
         count2 = (TextView) findViewById(R.id.count2);
-        count2.setText(value2+"");
+        count2.setText("수량: 1"); //초기 수량 임의 설정
         count3 = (TextView) findViewById(R.id.count3);
-        count3.setText(value3+"");
+        count3.setText("수량: 1"); //초기 수량 임의 설정
         plus1 = (ImageButton) findViewById(R.id.plus1);
         plus2 = (ImageButton) findViewById(R.id.plus2);
         plus3 = (ImageButton) findViewById(R.id.plus3);
         minus1 = (ImageButton) findViewById(R.id.minus1);
         minus2 = (ImageButton) findViewById(R.id.minus2);
         minus3 = (ImageButton) findViewById(R.id.minus3);
-        btnOrder = (ImageButton)findViewById(R.id.btnOrder);
+        //btnOrder = (ImageButton)findViewById(R.id.btnOrder);
 
         /*장바구니 버튼*/
         btnCart.setOnClickListener(new View.OnClickListener() {
@@ -71,94 +80,85 @@ public class CustomerCart extends AppCompatActivity {
         plus1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    value1++;
-                    count1.setText(value1+"");
+                value1++;
+                count1.setText("수량: " + value1 + "");
             }
         });
         plus2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 value2++;
-                count2.setText(value2+"");
+                count2.setText("수량: " + value2 + "");
             }
         });
         plus3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 value3++;
-                count3.setText(value3+"");
+                count3.setText("수량: " + value3 + "");
             }
         });
 
         minus1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                value1--;
-                count1.setText(value1+"");
+                if(value1 == 1){
+                    minus1.setActivated(false);
+                } else{
+                    value1--;
+                    count1.setText("수량: " + value1 + ""); }
             }
         });
         minus2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                value2--;
-                count2.setText(value2+"");
+                if(value2 == 1){
+                    minus2.setActivated(false);
+                } else{
+                    value2--;
+                    count2.setText("수량: " + value2 + ""); }
             }
         });
         minus3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                value3--;
-                count3.setText(value3+"");
+                if(value3 == 1){
+                    minus3.setActivated(false);
+                } else{
+                    value3--;
+                    count3.setText("수량: " + value3 + ""); }
             }
         });
 
-        /*btnDelete.setOnClickListener(new View.OnClickListener() {
+        button_delete1_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                menu1_1.setVisibility(View.INVISIBLE);
             }
-        });*/
+        });
 
-        btnOrder.setOnClickListener(new View.OnClickListener() {
+        button_delete1_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menu1_2.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        button_delete1_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menu1_3.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        /*btnOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(CustomerCart.this, Payment.class);
                 startActivity(intent);
                 Toast.makeText(CustomerCart.this, "결제를 진행합니다", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
-        /*하단바 메뉴 버튼들의 기능*/
-        home_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CustomerHome.class);
-                startActivity(intent);
-            }
-        });
-
-        group_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CustomerParticipate.class);
-                startActivity(intent);
-            }
-        });
-
-        delivery_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CustomerMyDelivery.class);
-                startActivity(intent);
-            }
-        });
-
-        profile_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CustomerMyPage.class);
-                startActivity(intent);
-            }
-        });
     }
 }
